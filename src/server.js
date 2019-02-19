19fbdcad7a9cef4c5119b73a17a461d8d068bec1
Nicolas Copia <nick.copia@gmail.com>
2019-02-19 13:32:22 -0600

    ready for test pub on heroku

---
+++ src/server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/heroku-meant-tut'))
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/heroku-meant-tut/index.html'))
+})
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080)
