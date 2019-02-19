dcd76473a688cac6a38bc51dad093fd3a9aeebd2
Nicolas Copia <nick.copia@gmail.com>
2019-02-19 15:17:43 -0600

    last try

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+const dist_dir = __dirname + '/dist/heroku-mean-tut'
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '../../dist/heroku-meant-tut/')))
+app.use(express.static(path.join(__dirname, '/dist/heroku-mean-tut')))
 console.log(`DEBUG: __dirname: ${__dirname}`)
 
 app.get('/*', function(req, res) {
-  console.log(`DEBUG: Final Path: ${path.join(__dirname, '../../dist/heroku-meant-tut/index.html')}`)
-  res.sendFile('index.html', { root: path.join(__dirname, '../../dist/heroku-meant-tut/') });
+  console.log(`DEBUG: Final Path: ${path.join(__dirname, '/dist/heroku-mean-tut')}`)
+  res.sendFile('index.html', { root: path.join(__dirname, '/dist/heroku-mean-tut') })
 })
 
 // Start the app by listening on the default Heroku port
