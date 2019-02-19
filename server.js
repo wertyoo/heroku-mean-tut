e8adeae5bae2e4890289b3f84c7cc7937479f790
Nicolas Copia <nick.copia@gmail.com>
2019-02-19 13:49:55 -0600

    removed __dirname and let pull from root instead:

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path')
 const app = express()
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/heroku-meant-tut'))
+app.use(express.static('/dist/heroku-meant-tut'))
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/heroku-meant-tut/index.html'))
+  res.sendFile(path.join('/dist/heroku-meant-tut/index.html'))
 })
 
 // Start the app by listening on the default Heroku port
